87f956f96fe1448db272491116e1f913325add74
Pyae Sone Aung <pyaesone.mdy95@gmail.com>
2025-07-08 05:17:03 +0800

    update

---
+++ food-app/babel.config.js
@@ -0,0 +1,14 @@
+// food-app/babel.config.js
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ["module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+      }]
+    ]
+  };
+};
